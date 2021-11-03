268398e232e0bad3a17d9de9a49b85ab2055566a
JCM01 <57594425+JCM01@users.noreply.github.com>
2021-11-03 13:00:13 +0100

    Add files via upload
    conexionmysql con python, creando desde docker

---
+++ conexionmysql.py
@@ -0,0 +1,36 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='sys',
+                                         user='root',
+                                         password='admin')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+    sql_select_Query = "select * from sys.cliente"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    # get all records
+    records = cursor.fetchall()
+    print("Total number of rows in table: ", cursor.rowcount)
+
+    print("\nPrinting each row")
+    for row in records:
+        print("idclientes= ", row[0], )
+        print("nombre = ", row[1])
+        print("ciudad  = ", row[2])
+
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
